1278ebf0a4fcb60c0407311a7949f73e259d8151
SilasRasch <silasrasch@gmail.com>
2023-05-02 12:27:01 +0200

    CreateUserPage page model userservice implementation

--- PhotographyBusiness/PhotographyBusiness/Pages/UserPages/CreateUserPage.cshtml.cs
+++ PhotographyBusiness/PhotographyBusiness/Pages/UserPages/CreateUserPage.cshtml.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PhotographyBusiness.Services.UserService;
 using System.ComponentModel.DataAnnotations;
 
 namespace PhotographyBusiness.Pages.UserPages
 {
     public class CreateUserPageModel : PageModel
     {
-        
+        private IUserService _userService;
         private PasswordHasher<string> passwordHasher;
         
         [BindProperty]
@@ -21,8 +22,9 @@ namespace PhotographyBusiness.Pages.UserPages
         [BindProperty, DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
 
-        public CreateUserPageModel()
+        public CreateUserPageModel(IUserService userService)
         {
+            _userService = userService;
             passwordHasher = new PasswordHasher<string>();
         }
 
@@ -32,7 +34,7 @@ namespace PhotographyBusiness.Pages.UserPages
             {
                 if (ModelState.IsValid)
                 {
-                    // UserService to create the new user
+                    _userService.CreateUser(new Models.User(Email, passwordHasher.HashPassword(null, Password), FullName, PhoneNumber));
                     return RedirectToPage("../Index");
                 }
                 return Page();
