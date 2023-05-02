8b69e05963f288bba63a9c6ec727766003578ec8
Mctaowing <mikkelchristensen2001@gmail.com>
2023-05-02 09:40:39 +0200

    Oprettede IService og implementerede i DBservice

--- PhotographyBusiness/PhotographyBusiness/Services/GenericDbService.cs
+++ PhotographyBusiness/PhotographyBusiness/Services/GenericDbService.cs
@@ -1,6 +1,35 @@
 ﻿namespace PhotographyBusiness.Services
 {
-    public class GenericDbService
+    public class GenericDbService<T> : IService<T> where T : class
     {
+        public Task AddObjectAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteObjectAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetObjectByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetObjectsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveObjects(List<T> objects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateObjectAsync(T obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- PhotographyBusiness/PhotographyBusiness/Services/IService.cs
+++ PhotographyBusiness/PhotographyBusiness/Services/IService.cs
@@ -1,6 +1,12 @@
 ﻿namespace PhotographyBusiness.Services
 {
-    public interface IService
+    public interface IService<T>
     {
+        Task<IEnumerable<T>> GetObjectsAsync();
+        Task AddObjectAsync(T obj);
+        Task DeleteObjectAsync(T obj);
+        Task UpdateObjectAsync(T obj);
+        Task<T> GetObjectByIdAsync(int id);
+        Task SaveObjects(List<T> objects);
     }
 }
