6a7161dc56f3fa1b8c3ab55c5dd97e708e94587f
arun1000lol <minkat030701@gmail.com>
2023-05-02 11:23:18 +0200

    Naming convention

--- PhotographyBusiness/PhotographyBusiness/MockData/MockUsers.cs
+++ PhotographyBusiness/PhotographyBusiness/MockData/MockUsers.cs
@@ -5,13 +5,13 @@ namespace PhotographyBusiness.MockData
 {
     public class MockUsers
     {
-        private static PasswordHasher<string> passwordHasher = new PasswordHasher<string>();
+        private static PasswordHasher<string> _passwordHasher = new PasswordHasher<string>();
 
         private static List<User> users = new List<User>()
         {
-            new User("admin@admin.com", passwordHasher.HashPassword(null, "admin"), "admin", "22256142"),
-            new User("123@123.com", passwordHasher.HashPassword(null, "123"), "123", "42424242"),
-            new User("tyler1@tyler1.com", passwordHasher.HashPassword(null, "tyler1"), "tyler1", "42791451")
+            new User("admin@admin.com", _passwordHasher.HashPassword(null, "admin"), "admin", "22256142"),
+            new User("123@123.com", _passwordHasher.HashPassword(null, "123"), "123", "42424242"),
+            new User("tyler1@tyler1.com", _passwordHasher.HashPassword(null, "tyler1"), "tyler1", "42791451")
         };
 
         public static List<User> GetMockUsers()
