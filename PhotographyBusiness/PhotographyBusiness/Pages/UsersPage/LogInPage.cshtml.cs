c101e5f1e0fe623d80e7d535a2dd09047839207b
Rasho-dk <103439939+Rasho-dk@users.noreply.github.com>
2023-05-03 17:19:03 +0200

    LogInPage with dropdown in frontPage

--- PhotographyBusiness/PhotographyBusiness/Pages/UsersPage/LogInPage.cshtml.cs
+++ PhotographyBusiness/PhotographyBusiness/Pages/UsersPage/LogInPage.cshtml.cs
@@ -17,8 +17,8 @@ namespace PhotographyBusiness.Pages.UsersPage
         public static User LoggedInUser { get; set; } = null;
         [BindProperty]
         [Required(ErrorMessage = "Please enter your email address.")]
-        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         [StringLength(100, ErrorMessage = "The email address must be no more than {1} characters long.")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Please enter a valid email address. exampel@exampel.com")]
         public string Email { get; set; }
         [BindProperty,DataType(DataType.Password)]
         [Required(ErrorMessage = "Please enter your password.")]
@@ -29,10 +29,7 @@ namespace PhotographyBusiness.Pages.UsersPage
         {
             this.userService = userService; 
         }
-        public void OnGet()
-        {
 
-        }
         public async Task<IActionResult> OnPost()
         {
             List<User> users = userService.GetAllUsers();   
@@ -48,8 +45,9 @@ namespace PhotographyBusiness.Pages.UsersPage
                         if (Email.Equals("admin")) claims.Add(new Claim(ClaimTypes.Role, "admin"));
                         
                         var claimsIdentity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+                        //TODO 
                         //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                        return RedirectToAction("../Pages/Index");
+                        return RedirectToPage("/Index");
                     }
                 }    
             }
