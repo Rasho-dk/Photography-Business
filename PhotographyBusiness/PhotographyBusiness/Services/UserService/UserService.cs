bfe22a0914562546a447a59b5f7bb239081d4c75
arun1000lol <minkat030701@gmail.com>
2023-05-04 14:54:48 +0200

    meow

--- PhotographyBusiness/PhotographyBusiness/Services/UserService/UserService.cs
+++ PhotographyBusiness/PhotographyBusiness/Services/UserService/UserService.cs
@@ -43,7 +43,7 @@ namespace PhotographyBusiness.Services.UserService
 
         public List<User> GetAllUsers()
         {
-            return Users;
+            return Users; 
 
         }
         public async Task<User> GetUserByIdAsyn(int id)
