124f4075fed0dc0d30eac02bb22c836740450bf2
Rasho-dk <103439939+Rasho-dk@users.noreply.github.com>
2023-05-04 23:41:40 +0200

    LogInd Test

--- PhotographyBusiness/PhotographerTest/TestLogIn.cs
+++ PhotographyBusiness/PhotographerTest/TestLogIn.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
+using PhotographyBusiness.Models;
 using PhotographyBusiness.Pages.AccountPages;
 using PhotographyBusiness.Services.UserService;
+using System.Security.Authentication;
 
 namespace PhotographerTest
 {
@@ -22,28 +24,38 @@ namespace PhotographerTest
 
         }
         [TestMethod]
+        //[ExpectedException(typeof(FormatException))]
         public void Login_UnHappyPath()
         {
             var userservicemock = new Mock<IUserService>();
             var model = new LogInPageModel(userservicemock.Object)
             {
                 Email = "Test@Test.com",
-                Password = "12345678"
+                Password = "1234567890"
             };
 
+            var users = new List<User>()
+            {
+                new User() {Name = "Test",Password="123456789",Email="Test@Test.com"},
+                //new User(){Name = "admin",Password ="admin123",Email="admin@admin.dk"}
+            };
 
-            var result = model.OnPost();
             try
             {
-                Assert.AreEqual("123", model.Email);
-                Assert.AreEqual("3123", model.Password);
+                userservicemock.Setup(x => x.GetAllUsers()).Returns(users);
+                var ex = Assert.ThrowsException<Exception>(() => model.OnPostAsync());
             }
             catch (Exception ex)
             {
-               // Assert.AreEqual("Invalid email or password.Please try again", ex.Message);
+                Assert.AreEqual("Invalid email or password.Please try again", model.DisplayMessage);
             }
 
-        }
 
+
+
+
+
+
+        }
     }
 }
--- PhotographyBusiness/PhotographyBusiness/Pages/AccountPages/LogInPage.cshtml.cs
+++ PhotographyBusiness/PhotographyBusiness/Pages/AccountPages/LogInPage.cshtml.cs
@@ -12,7 +12,7 @@ namespace PhotographyBusiness.Pages.AccountPages
 {
     public class LogInPageModel : PageModel
     {
-        public IUserService userService; 
+        public IUserService userService;
         //Kun  en bruger i brug
         //public static User LoggedInUser { get; set; } = null;
         [BindProperty]
@@ -20,49 +20,48 @@ namespace PhotographyBusiness.Pages.AccountPages
         [StringLength(100, ErrorMessage = "The email address must be no more than {1} characters long.")]
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Please enter a valid email address. exampel@exampel.com")]
         public string Email { get; set; }
-        [BindProperty,DataType(DataType.Password)]
+        [BindProperty, DataType(DataType.Password)]
         [Required(ErrorMessage = "Please enter your password.")]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "The password must be between 8 and 20 characters long.")]
-        public string? Password { get; set; }    
+        public string? Password { get; set; }
         public string DisplayMessage { get; set; }
         public LogInPageModel(IUserService userService)
         {
-            this.userService = userService; 
+            this.userService = userService;
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            
-                List<User> users = userService.GetAllUsers();
-                foreach (var user in users)
-                {
+
+            List<User> users = userService.GetAllUsers();
+            foreach (var user in users)
+            {
                 // if (Email == user.Email) // Hvis man ville bruge email til Claim
-                    if (user.Name.Equals(user.Name))
+                if (user.Name.Equals(user.Name))
+                {
+                    var passwordHasher = new PasswordHasher<string>();
+                    try
                     {
-                        var passwordHasher = new PasswordHasher<string>();
-                        try
+                        if (passwordHasher.VerifyHashedPassword(null, user.Password, Password) == PasswordVerificationResult.Success)
                         {
-                            if (passwordHasher.VerifyHashedPassword(null, user.Password, Password) == PasswordVerificationResult.Success)
-                            {
-                                //LoggedInUser = user;
-                                var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Name) }; // Ændret email til user.Name
-                                if (user.Name.Equals("admin")) claims.Add(new Claim(ClaimTypes.Role, "admin"));  // Arun: Betyder så også at Jacks admin User kommer til at have navnet "Admin"
-                                                                                                                 // if (user.Email == "EXAMPLE@jacksphotography.co.uk") claims.Add(new Claim(ClaimTypes.Role, "admin")) <-- Hvis vi hellere ville bruge email.
+                            //LoggedInUser = user;
+                            var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Name) }; // Ændret email til user.Name
+                            if (user.Name.Equals("admin")) claims.Add(new Claim(ClaimTypes.Role, "admin"));  // Arun: Betyder så også at Jacks admin User kommer til at have navnet "Admin"
+                                                                                                             // if (user.Email == "EXAMPLE@jacksphotography.co.uk") claims.Add(new Claim(ClaimTypes.Role, "admin")) <-- Hvis vi hellere ville bruge email.
 
-                                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                                return RedirectToPage("/Index");
-                            }
+                            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                            return RedirectToPage("/Index");
                         }
-                        catch (Exception ex) 
-                        {
-                                 DisplayMessage = "Invalid email or password.Please try again";
-                        }
+                    }
+                    catch (Exception ex)
+                    {
 
-
-                }                    
+                        DisplayMessage = "Invalid email or password.Please try again";
+                    }
                 }
+            }
             DisplayMessage = "Invalid email or password.Please try again";
 
             return Page();
