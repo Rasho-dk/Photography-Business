201d304a899b5c5ed922b968b96aa9483094b2ec
SilasRasch <silasrasch@gmail.com>
2023-05-04 10:57:19 +0200

    Added Booking-class & DbContext DbSet
    Added Booking-class and DbSet in ObjectDbContext for booking and user-classes.

--- PhotographyBusiness/PhotographyBusiness/EFDbContext/ObjectDbContext.cs
+++ PhotographyBusiness/PhotographyBusiness/EFDbContext/ObjectDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PhotographyBusiness.Models;
 
 namespace PhotographyBusiness.EFDbContext
 {
@@ -9,6 +10,9 @@ namespace PhotographyBusiness.EFDbContext
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PhotographerDB ;Integrated Security=True;Connect Timeout=30;");
         }
         // TODO Der skal oprettes DbSet<T> get set til at konekte med Database
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
 
     }
 }
