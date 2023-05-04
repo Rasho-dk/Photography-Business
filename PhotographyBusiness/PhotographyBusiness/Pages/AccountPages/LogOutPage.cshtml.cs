7e5a74e8a31bb2562a26e01b4ec96c2b0f96388d
Mctaowing <mikkelchristensen2001@gmail.com>
2023-05-04 09:06:05 +0200

    log out page

---
+++ PhotographyBusiness/PhotographyBusiness/Pages/AccountPages/LogOutPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotographyBusiness.Pages.AccountPages
+{
+    public class LogOutPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
