0ce9065b294cc4ac079c0cb57675be40517c2622
Mctaowing <mikkelchristensen2001@gmail.com>
2023-05-04 14:14:44 +0200

    efe

--- PhotographyBusiness/PhotographyBusiness/Pages/BookingPages/CreateBookingRequestPage.cshtml.cs
+++ PhotographyBusiness/PhotographyBusiness/Pages/BookingPages/CreateBookingRequestPage.cshtml.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-<<<<<<< HEAD
 using PhotographyBusiness.Models;
-=======
->>>>>>> 649b07b27c427f5600eccbebf68f23a0ef347265
 
 namespace PhotographyBusiness.Pages.BookingPages
 {
     public class CreateBookingRequestPageModel : PageModel
     {
-<<<<<<< HEAD
         [BindProperty] public string Name { get; set; }
         [BindProperty] public string Email { get; set; }
         [BindProperty] public string Phone { get; set; }
@@ -18,8 +14,6 @@ namespace PhotographyBusiness.Pages.BookingPages
         [BindProperty] public string StreetName { get; set; }
         public Booking Booking { get; set; }
 
-=======
->>>>>>> 649b07b27c427f5600eccbebf68f23a0ef347265
         public void OnGet()
         {
         }
