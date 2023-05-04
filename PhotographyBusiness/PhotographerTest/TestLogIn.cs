38336f7e9e98626a51e6048725ab167a5882e757
Rasho-dk <103439939+Rasho-dk@users.noreply.github.com>
2023-05-04 14:58:12 +0200

    unit test

--- PhotographyBusiness/PhotographerTest/TestLogIn.cs
+++ PhotographyBusiness/PhotographerTest/TestLogIn.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using PhotographyBusiness.Pages.AccountPages;
+using PhotographyBusiness.Services.UserService;
 
 namespace PhotographerTest
 {
+    [TestClass]
     public class TestLogIn
     {
+        private IUserService _userService;
+        [TestMethod]
+        public void Login_Test_HappyPath()
+        {
+            var userservicemock = new Mock<IUserService>();
+            var model = new LogInPageModel(userservicemock.Object)
+            {
+                Email = "Test@Test.com",
+                Password = "12345678"
+            };
+            Assert.AreEqual("12345678", model.Password);
+            Assert.AreEqual("Test@Test.com", model.Email);
+
+        }
+        [TestMethod]
+        public void Login_UnHappyPath()
+        {
+            var userservicemock = new Mock<IUserService>();
+            var model = new LogInPageModel(userservicemock.Object)
+            {
+                Email = "Test@Test.com",
+                Password = "12345678"
+            };
+            try
+            {
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("Error, invalid email or password.Please try again",ex.Message);
+            }
+
+        }
 
     }
 }
