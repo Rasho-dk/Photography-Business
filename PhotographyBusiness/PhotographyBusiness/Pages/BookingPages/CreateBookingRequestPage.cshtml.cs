025eb7f9a430f5676c17bbba2574d6f84a24005f
Mctaowing <mikkelchristensen2001@gmail.com>
2023-05-04 15:10:39 +0200

    started on CreateBookingRequestPage

--- PhotographyBusiness/PhotographyBusiness/Pages/BookingPages/CreateBookingRequestPage.cshtml.cs
+++ PhotographyBusiness/PhotographyBusiness/Pages/BookingPages/CreateBookingRequestPage.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PhotographyBusiness.Models;
+using PhotographyBusiness.Services.BookingService;
+using PhotographyBusiness.Services.UserService;
 
 namespace PhotographyBusiness.Pages.BookingPages
 {
     public class CreateBookingRequestPageModel : PageModel
     {
+        private IBookingService _bookingService;
+        private IUserService _userService;
         [BindProperty] public string Name { get; set; }
         [BindProperty] public string Email { get; set; }
         [BindProperty] public string Phone { get; set; }
@@ -13,9 +17,18 @@ namespace PhotographyBusiness.Pages.BookingPages
         [BindProperty] public string ZipCode { get; set; }
         [BindProperty] public string StreetName { get; set; }
         public Booking Booking { get; set; }
+        public User User { get; set; }
 
-        public void OnGet()
+        public CreateBookingRequestPageModel(IBookingService bookingService, IUserService userService)
         {
+            _bookingService = bookingService;
+            _userService = userService;
+        }
+
+        public async IActionResult OnGet()
+        {
+            var user = await _userService.GetUserByEmailAsync(User.Email);
+            return Page();
         }
     }
 }
