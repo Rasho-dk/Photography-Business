649b07b27c427f5600eccbebf68f23a0ef347265
Mctaowing <mikkelchristensen2001@gmail.com>
2023-05-04 13:47:47 +0200

    bookingrequest page

---
+++ PhotographyBusiness/PhotographyBusiness/Pages/BookingPages/CreateBookingRequestPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PhotographyBusiness.Pages.BookingPages
+{
+    public class CreateBookingRequestPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
