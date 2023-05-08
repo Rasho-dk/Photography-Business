8db4a915975c22e05f3366cc1b37a8c80cf4ae8f
Rasho-dk <103439939+Rasho-dk@users.noreply.github.com>
2023-05-08 09:26:09 +0200

    Login

--- PhotographyBusiness/PhotographyBusiness/Pages/AccountPages/LogInPage.cshtml.cs
+++ PhotographyBusiness/PhotographyBusiness/Pages/AccountPages/LogInPage.cshtml.cs
@@ -13,7 +13,7 @@ namespace PhotographyBusiness.Pages.AccountPages
     public class LogInPageModel : PageModel
     {
         public IUserService userService;
-        //Kun  en bruger i brug
+        //Kun en bruger i brug
         //public static User LoggedInUser { get; set; } = null;
         [BindProperty]
         [Required(ErrorMessage = "Please enter your email address.")]
@@ -29,6 +29,35 @@ namespace PhotographyBusiness.Pages.AccountPages
         {
             this.userService = userService;
         }
+        #region
+        /// <summary>
+        /// Used for Unit test
+        ///This is for log in a user with a given email og password
+        /// </summary>
+        /// <param name="email">Email of the user to log in</param>
+        /// <param name="password">Password of the user to log in</param>
+        /// <returns>True if the user successfully logged in, false otherwise</returns>
+        //public bool Login(string email, string password)
+        //{
+        //    List<User> users = userService.GetAllUsers();
+        //    foreach(var user in users) 
+        //    {
+        //        if(user.Email == email && user.Password == Password)
+        //            return true;
+        //    }
+        //    return false;
+        //}
+        #endregion
+
+
+
+
+        /// <summary>
+        /// This method must first retrieve the list of the Users via _userService and checl whether the user with (Eamil,Password) exists in the lsit
+        /// If the user email exist and the password is match,a new list of the Claim objects is created which is initialized with a new Claim containing UserName.
+        /// And if the user is admin new claim containing a "admin" and add to the list of the Claim with ClaimTypes.Role.
+        /// Then ClaimIdentity object is created with our new list of Claims and a SignIn is made with the CalimsIdentity.
+        /// <returns>If Login pass redurect to page ("/Index"), otherwise errore message dispaly </returns>
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -36,14 +65,14 @@ namespace PhotographyBusiness.Pages.AccountPages
             List<User> users = userService.GetAllUsers();
             foreach (var user in users)
             {
-                 if (Email == user.Email) // Hvis man ville bruge email til Claim
+                 if (Email.ToLower() == user.Email.ToLower()) // Hvis man ville bruge email til Claim
                 //if (user.Name.Equals(user.Name))
                 {
                     var passwordHasher = new PasswordHasher<string>();
                     try
                     {
-                        if (passwordHasher.VerifyHashedPassword(null, user.Password, Password) == PasswordVerificationResult.Success)
-                        //if (user.Password.Equals(Password))
+                       // if (passwordHasher.VerifyHashedPassword(null, user.Password, Password) == PasswordVerificationResult.Success)
+                        if (user.Password.Equals(Password))
                         {
                             //LoggedInUser = user;
                             var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Name) }; // Ændret email til user.Name
@@ -52,7 +81,7 @@ namespace PhotographyBusiness.Pages.AccountPages
 
                             var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                            //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
                             return RedirectToPage("/Index");
                         }
                     }
